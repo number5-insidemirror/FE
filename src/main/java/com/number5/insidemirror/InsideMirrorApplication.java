c26842b892ea24477315ac647c81d40c2287ea82
Suhyeon7 <zxguszx@naver.com>
2024-12-06 16:42:57 +0900

    first commit

---
+++ src/main/java/com/number5/insidemirror/InsideMirrorApplication.java
@@ -0,0 +1,13 @@
+package com.number5.insidemirror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsideMirrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsideMirrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/number5/insidemirror/InsideMirrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.number5.insidemirror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsideMirrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
