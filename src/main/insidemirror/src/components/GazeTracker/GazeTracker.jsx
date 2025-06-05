2320d4f9f0923852b4b13746ddc50cb9fad01649
chldsbdud <cyy2532@gmail.com>
2025-06-05 15:30:10 +0900

    ✨ Feat:경로에 ws 추가
    Feat:경로에 ws 추가

--- src/main/insidemirror/src/components/GazeTracker/GazeTracker.jsx
+++ src/main/insidemirror/src/components/GazeTracker/GazeTracker.jsx
@@ -5,7 +5,7 @@ function GazeTracker({ onGaze }) {
     let socket;
 
     const connect = () => {
-      socket = new WebSocket(`${process.env.REACT_APP_WEBSOCKET_API_BASE_URL}`);
+      socket = new WebSocket(`${process.env.REACT_APP_WEBSOCKET_API_BASE_UR}/ws`);
 
       socket.onopen = () => {
         console.log("WebSocket connected");
